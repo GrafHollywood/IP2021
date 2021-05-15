0234e538edd1f132d1a9e2ca6b5f7e5970564752
Matthew Kalinin <matvey.kalinin2001@gmail.com>
2021-05-15 23:48:00 +0300

    удалено представления из бэка

--- back-app/routes/index.js
+++ back-app/routes/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 const connection = require('../mode/db');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   data = await connection.query('SELECT * FROM Valve_Model');
   console.log(data[0]);
   res.send(data[0]);
-  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
